88fd905f5a1ee6cb50cae874ab5da1cc59b73cdc
Steven Gutstein <s.m.gutstein@gmail.com>
2018-06-19 12:17:46 -0500

    Init commit

---
+++ Cifar10_Ensemble/Cifar10_Accessor/readCifar10.py
@@ -0,0 +1,58 @@
+import cPickle
+import matplotlib.pyplot as plt
+import os
+
+default_path = '/media/innovationcommons/DataStorage/Cifar-10/cifar-10-batches-py'
+
+def getCifar10(file, reshape_data = False, path = default_path):
+    with open(os.path.join(default_path,file), 'rb') as f:
+        cifar_dict = cPickle.load(f)
+        if reshape_data:
+            num_images = cifar_dict['data'].shape[0]
+            temp = cifar_dict['data'].reshape(num_images,3,32,32)
+            cifar_dict['data'] = temp.transpose([0,2,3,1])
+    return cifar_dict
+
+def getMetaDict(path = default_path):
+    with open(os.path.join(default_path,'batches.meta'), 'rb') as f:
+        label_dict = cPickle.load(f)
+    return label_dict
+
+def showImage(img, label=None):
+    plt.imshow(img)
+    plt.show()
+
+
+
+'''
+>>> z=getCifar10('data_batch_1')
+>>> type(z)
+<type 'dict'>
+>>> z.keys()
+['data', 'labels', 'batch_label', 'filenames']
+
+>>> type(z['data'])
+<type 'numpy.ndarray'>
+>>> z['data'].shape
+(10000, 3072)
+
+>>> type(z['labels'])
+<type 'list'>
+>>> len(z['labels'])
+10000
+
+>>> type(z['batch_label'])
+<type 'str'>
+>>> z['batch_label']
+'training batch 1 of 5'
+
+>>> type(z['filenames'])
+<type 'list'>
+>>> len(z['filenames'])
+10000
+
+
+>>> zz=getMetaDict()
+>>> zz
+{'num_cases_per_batch': 10000, 'label_names': ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'], 'num_vis': 3072}
+'''
---
+++ Cifar10_Ensemble/Cifar10_Classifiers/RandomForest.py
@@ -0,0 +1,141 @@
+from . import *
+from sklearn.ensemble import RandomForestClassifier
+
+
+class Random_Forest_Classifier:
+    
+    cfg_spec_file = 'rfc_configspec.cfg'
+    
+    def __init__(self, cfg_file):
+
+        config = read_cfg_file(cfg_file,
+                               Random_Forest_Classifier.cfg_spec_file)
+        param_dict = config['Parameters']
+        for curr_param in param_dict:
+            self.__dict__[curr_param] = param_dict[curr_param]
+        
+        self.has_train_data = False
+        self.has_test_data = False
+        self.train_acc = None
+        self.test_acc = None
+        self.train_conf_matrix = None
+        self.test_conf_matrix = None        
+
+        self.label_names = getMetaDict()['label_names']
+        self.num_classes = len(self.label_names)
+        self.abbr_names =[x[0:5] for x in self.label_names]
+        self.summary_dict = {'name':self.name, 'type':self.__class__.__name__}
+
+        self.classifier = None
+
+
+    def create(self):
+        self.classifier = RandomForestClassifier(self.num_estimators)
+        self.summary_dict['classifier'] = self.classifier
+
+    def read(self):
+        saved_file = os.path.join(results_path, self.name + '.pkl')
+        self.summary_dict = pickle.load(open(saved_file, 'rb'))
+        for curr_field in self.summary_dict:
+            self.__dict__[curr_field] = self.summary_dict[curr_field]
+        print "Read ", saved_file
+ 
+    def save(self):
+        outfile = os.path.join(results_path, self.name + '.pkl')
+        pickle.dump(self.summary_dict,
+                    open(outfile, 'wb'))
+        print "Saved to ",outfile
+
+    def get_train_data(self):
+        cifar_dict = getCifar10(self.train_data_file)
+        self.train_data = cifar_dict['data']
+        self.train_labels = np.asarray(cifar_dict['labels'])
+        self.has_train_data = True
+        
+    def get_test_data(self):
+        cifar_dict = getCifar10(self.test_data_file)
+        self.test_data = cifar_dict['data']
+        self.test_labels = np.asarray(cifar_dict['labels'])
+        self.has_test_data = True
+        
+    def fit(self):
+        if not self.has_train_data:
+            print "Can't fit function without training data"
+            return
+        
+        print "Fitting %s"%(self.name)
+        self.classifier.fit(self.train_data, self.train_labels)
+        self.summary_dict['classifier'] = self.classifier        
+
+    def get_metrics(self):
+        self.train_acc = self.classifier.score(self.train_data,
+                                               self.train_labels)
+        self.train_conf_matrix = self.get_conf_matrix(self.train_data,
+                                                      self.train_labels)
+        self.summary_dict['train_acc'] = self.train_acc
+        self.summary_dict['train_conf_matrix'] = self.train_conf_matrix
+
+        
+        if self.has_test_data:
+            self.test_acc = self.classifier.score(self.test_data,
+                                                  self.test_labels)
+            self.test_conf_matrix = self.get_conf_matrix(self.test_data,
+                                                         self.test_labels)
+            self.summary_dict['test_acc'] = self.test_acc
+            self.summary_dict['test_conf_matrix'] = self.test_conf_matrix
+
+    def classify(self, sample):
+        return  self.classifier.predict_proba(sample)
+
+    def get_conf_matrix(self, data, true_labels):
+        pred_labels = self.classifier.predict(data)
+        if len(true_labels) != len(pred_labels):
+            print "Error, the number of true labels != number of predicted labels"
+            return
+        conf_matrix = np.zeros((len(set(true_labels)),
+                                len(set(true_labels))))
+        class_dist = np.zeros((len(set(true_labels))))
+        for ctr in range(len(true_labels)):
+            x = true_labels[ctr]
+            y = pred_labels[ctr]
+            conf_matrix[x,y] += 1
+            class_dist[x] += 1
+
+        for ctr in range(len(class_dist)):
+            conf_matrix[ctr,:] /= class_dist[ctr]
+            
+        return conf_matrix
+
+    def print_conf_matrix(self, trvate="train"):
+        if trvate.lower() == "train":
+            conf_matrix = self.train_conf_matrix
+            out_label = "Confusion Matrix - Training Set"
+        else:
+            conf_matrix = self.test_conf_matrix
+            out_label = "Confusion Matrix - Testing Set"
+
+        if conf_matrix is not None:
+            df = pd.DataFrame(conf_matrix, index=self.abbr_names,
+                              columns = self.abbr_names)
+            df = df.applymap("{0:.3f}".format)
+            print '\n',df,'\n'
+        else:
+            print "Unknown"
+
+    def print_acc(self):
+        print "Training Acc:", 
+        if self.train_acc:
+            print self.train_acc
+        else:
+            print "Unknown"
+
+        print "Testing Acc:", 
+        if self.test_acc:
+            print self.test_acc
+        else:
+            print "Unknown"
+
+        
+
+        
+        
